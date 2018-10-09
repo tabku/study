917e3bd5b02bdb084a4919b2ccce88c45fa28f43
tabku <41511147+tabku@users.noreply.github.com>
2018-10-09 20:30:25 +0900

    デプロイのため、ファイルを移動
    herokuデプロイのため、index.phpを指定github直下に置く必要がある。

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
-<?
-echo 'phpのテストです。<br>';
-$dbinfo = parse_url(getenv('DATABASE_URL'));
-$dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
-$pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
-phpinfo();
-?>
+<?php require_once('data.php'); ?>
+
+<!DOCTYPE html>
+<html lang=ja>
+  <head>
+    <meta charset="utf-8">
+    <title>Tour</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  </head>
+  <body>
+  <div class="container tour-wrapper">
+    <form action="tour-confirm.php" method="post">
+      <div class="tour-items">
+            <!-- ツアーの名前と日程ををforeachで出力する -->
+          <?php foreach ($tours as $tour): ?>
+            <div class="tour-item">
+              <p><?php echo $tour->getName() ?></p>
+              <img src="<?php echo $tour->getImg() ?>">
+              <p><?php echo $tour->getDate() ?></p>
+              <p><?php echo $tour->getTaxInPrice() ?>万円（税込み）</p>
+              <input type="text" name="<?php echo $tour->getName() ?>" value=0>
+              <span>人</span>
+            </div>
+          <?php endforeach ?>
+      </div>
+      <input type="submit" value="予約する">
+    </form>
+  </div>
+
+  </body>
+</html>
