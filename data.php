b4d4bbb0426af75e2b48a1a4271a5e5d28567fea
tabku <41511147+tabku@users.noreply.github.com>
2018-10-12 22:24:22 +0900

    Update data.php

--- data.php
+++ data.php
@@ -5,7 +5,7 @@ require_once('tour.php');
 //インスタンスを生成＆変数に代入
   $china = new Tour('CHINA',20,'4月4日',"tour-img/china.jpg");
   $korea = new Tour('KOREA',30,'4月5日',"tour-img/korea.jpg");
-  $thai = new Tour('THAI',40,'4月6日',"tour-img/thai.jpg");
+  $thai = new Tour('GUNMA',40,'4月6日',"tour-img/thai.jpg");
   $okinawa = new Tour('OKINAWA',50,'4月7日',"tour-img/okinawa.jpg");
 
 
